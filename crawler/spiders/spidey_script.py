1906e8045e5971fe689521ecdbd086a1c4c78428
Alex Neshyba <alexnesh@microsoft.com>
2018-11-10 03:53:24 -0800

    Mongo init, default recipe site

--- crawler/spiders/spidey_script.py
+++ crawler/spiders/spidey_script.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import urllib
+from pymongo import MongoClient
 from bs4 import BeautifulSoup 
 from recipe_parser import parse_recipe
+
+DEFAULT_RECIPE_URL = 'https://www.tasteofhome.com/recipes/'
 
 class RecipeSpiderSpider(scrapy.Spider):
     name = 'spidey_script'
 
+    def __init__(self, db_name="spidey_data", *args, **kwargs):
+        super(RecipeSpiderSpider, self).__init__(*args, **kwargs)
+        client = MongoClient()
+        db = client[db_name]
+        self.mongo = db["recipes"]
+
     def start_requests(self):
-        yield scrapy.Request(self.url)
+        if hasattr(self, 'url'):
+            yield scrapy.Request(self.url)
+        yield scrapy.Request(DEFAULT_RECIPE_URL)
 
     def parse(self, response):
         urls = []
@@ -24,4 +35,3 @@ class RecipeSpiderSpider(scrapy.Spider):
                     recipe = parse_recipe(url, self.debug)
                     if (recipe):
                         yield recipe
-
