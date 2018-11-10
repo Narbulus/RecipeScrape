514998686b8b9d402de2c433af9e5f1860b429b7
Spencer H. Anderson <spenhand@fb.com>
2018-11-10 02:24:14 -0800

    Working

--- crawler/spiders/recipe_spider.py
+++ crawler/spiders/recipe_spider.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import scrape_recipe
+import urllib
 from bs4 import BeautifulSoup 
 from pymongo import MongoClient
 
 
 class RecipeSpiderSpider(scrapy.Spider):
     name = 'recipe_spider'
-    start_urls = ['https://www.tasteofhome.com/recipes/']
+    #start_urls = ['https://www.tasteofhome.com/recipes/']
+    start_urls = ['https://www.maangchi.com/recipes']
     client = MongoClient()
     db = client.recipe_db_test
     recipe_collection = db.recipes
 
     def parse(self, response):
+        urls = []
         print(response.url)
         html = BeautifulSoup(response.body, 'html.parser')
-        for recipe_item in html.find_all('li'):
-            for recipe_link in recipe_item.find_all('a'):
-                recipe_url = recipe_link['href']
-                if (recipe_url):
-                    print("Parsing URL " + recipe_url)
-                    scrape_recipe.parse_recipe(recipe_url)
+        for link in html.find_all('a'):
+            url = link['href']
+            if (url):
+                url = urllib.parse.urljoin(response.url, url)
+                if (url not in urls):
+                    print("Parsing URL " + url)
+                    scrape_recipe.parse_recipe(url)
+                    urls.append(url)
         
